ab5c72c3b9d5eaf8666d9da931b2ff05f646d524
FireGamer3 <gm31334@gmail.com>
2022-03-05 09:00:16 -0600

    React Router Initial Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./routes/index";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-      </header>
+    <div class="dark">
+      <div class="bg-white dark:bg-gray-900 dark:text-white">
+        <div class="p-8 max-w-7xl mx-auto min-h-screen">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   );
 }
